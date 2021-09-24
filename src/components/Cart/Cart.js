10b1541e92b1272550d00cf0da38d8baa7e6154c
TakiKauser <takikauserbadhon@gmail.com>
2021-09-25 02:48:25 +0600

    reduce

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,10 +4,16 @@ import './Cart.css';
 const Cart = (props) => {
     // console.log(props.cart);
     const {cart} = props;
+
+    const totalReducer = (previous, product) => previous + product.price;
+    const total = cart.reduce(totalReducer, 0);
+
+    /* 
     let total = 0;
     for(const product of cart){
         total = total + product.price;
-    }
+    } 
+    */
     return (
         <div>
             <h3>Order Summary</h3>
