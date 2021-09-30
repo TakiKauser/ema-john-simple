c1911ff84fe900579e33e931e1dc442fc7b2e247
TakiKauser <takikauserbadhon@gmail.com>
2021-10-01 01:21:17 +0600

    useCart

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
+import Cart from '../Cart/Cart';
 import './OrderReview.css'
 
 const OrderReview = () => {
     const [products] = useProducts();
+    const [cart] = useCart(products);
     return (
-        <div>
-            <h2>Order Review</h2>
+        <div className="shop-container">
+            <div className="product-container">
+
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart} />
+            </div>
+            {/* <h2>Order Review</h2>
             <h3>{products.length}</h3>
+            <h3>{cart.length}</h3>
+            <Cart cart={cart}/> */}
         </div>
     );
 };
