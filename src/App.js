d2e3a0b8c0ed38323e8b01723b7510bf5fb0ea01
TakiKauser <takikauserbadhon@gmail.com>
2021-10-01 00:35:42 +0600

    simple-route-setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 // import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
+import OrderReview from './components/OrderReview/OrderReview';
 import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Shop />
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Shop />
+          </Route>
+          <Route path="/shop">
+            <Shop />
+          </Route>
+          <Route path="/review">
+            <OrderReview />
+          </Route>
+          <Route path="/inventory">
+            <Inventory />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
